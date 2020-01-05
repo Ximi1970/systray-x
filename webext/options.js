c1d1bf40cd374739bb6493337090d650725d6717
Ximi1970 <ximi.github@gmail.com>
2020-01-05 23:28:42 +0100

    Add radiobutton en checkbox save test

--- webext/options.js
+++ webext/options.js
@@ -1,23 +1,46 @@
 function saveOptions(e) {
   e.preventDefault();
+
+  console.debug("Save preferences");
+  
   browser.storage.sync.set({
-//    color: document.querySelector("#color").value
+    optionsRadioTest: document.querySelector('input[name="options_test"]:checked').value,
+    optionsCheck1: document.querySelector('input[name="check1"]').checked,
+    optionsCheck2: document.querySelector('input[name="check2"]').checked,
+    optionsCheck3: document.querySelector('input[name="check3"]').checked
   });
 }
 
 function restoreOptions() {
 
-  function setCurrentChoice(result) {
-//    document.querySelector("#color").value = result.color || "blue";
+  console.debug("Restore preferences");
+
+  function setCurrentRadioChoice(result) {
+    let selector = result.optionsRadioTest || "Option1";
+    let radioButton = document.querySelector(`[value=${selector}]`)
+    radioButton.checked = true;
+  }
+
+  function setCurrentCheckChoice(result) {
+    let checkbox1 = document.querySelector('[name="check1"]');
+    checkbox1.checked = result.optionsCheck1 || false;;
+    let checkbox2 = document.querySelector('[name="check2"]');
+    checkbox2.checked = result.optionsCheck2 || false;;
+    let checkbox3 = document.querySelector('[name="check3"]');
+    checkbox3.checked = result.optionsCheck3 || false;;
   }
 
   function onError(error) {
     console.log(`Error: ${error}`);
   }
 
-  var getting = browser.storage.sync.get("color");
-  getting.then(setCurrentChoice, onError);
+  var getting = browser.storage.sync.get("optionsRadioTest");
+  getting.then(setCurrentRadioChoice, onError);  
+  
+  var getting = browser.storage.sync.get(["optionsCheck1", "optionsCheck2", "optionsCheck3"]);
+  getting.then(setCurrentCheckChoice, onError);  
+
 }
 
 document.addEventListener("DOMContentLoaded", restoreOptions);
-document.querySelector("form").addEventListener("submit", saveOptions);
+document.querySelector('[name="saveform"]').addEventListener("submit", saveOptions);
