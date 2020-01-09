5134cf83fa6e830263f15796b75e5c71675db56b
Ximi1970 <ximi.github@gmail.com>
2020-01-09 23:37:33 +0100

    Add included account tree

--- webext/js/options_accounts.js
+++ webext/js/options_accounts.js
@@ -35,7 +35,9 @@ SysTrayX.Accounts = {
       console.debug("SysTrayX accounts name: " + mailAccount[i].name);
       console.debug("SysTrayX accounts type: " + mailAccount[i].type);
 
-      accounts[mailAccount[i].type] = [];
+      if (!accounts[mailAccount[i].type]) {
+        accounts[mailAccount[i].type] = [];
+      }
       accounts[mailAccount[i].type].push({
         id: mailAccount[i].id,
         name: mailAccount[i].name
@@ -48,25 +50,54 @@ SysTrayX.Accounts = {
       console.debug(prop + ": " + accounts[prop][0].name);
     }
 
-    let p = document.getElementsByClassName("account");
-    p[0].innerHTML = accounts.imap[0].name;
+    /*
+     *  Build tree
+     */
 
     // Get base
-    let treeBase = document.getElementsByClassName("accountstree");
+    let treeBase = document.getElementById("accountsTree");
 
     for (let prop in accounts) {
-      console.debug("Tree: " + prop + ": " + accounts[prop][0].name);
-
       let typeLi = document.createElement("li");
 
       let typeSpan = document.createElement("span");
-      typeSpan.setAttribute("class", "caret");
+      if (accounts[prop]) {
+        typeSpan.setAttribute("class", "caret");
+      }
       let typeText = document.createTextNode(prop);
       typeSpan.appendChild(typeText);
-
       typeLi.appendChild(typeSpan);
 
-      treeBase[0].appendChild(typeLi);
+      if (accounts[prop]) {
+        let typeUl = document.createElement("ul");
+        typeUl.setAttribute("class", "nested");
+
+        for (let i = 0; i < accounts[prop].length; ++i) {
+          let typeLi = document.createElement("li");
+          let typeInput = document.createElement("input");
+          typeInput.setAttribute("type", "checkbox");
+          typeInput.setAttribute("name", accounts[prop][i].name);
+          typeInput.setAttribute("value", accounts[prop][i].id);
+          typeLi.appendChild(typeInput);
+          let typeText = document.createTextNode(" " + accounts[prop][i].name);
+          typeLi.appendChild(typeText);
+          typeUl.appendChild(typeLi);
+        }
+        typeLi.appendChild(typeUl);
+      }
+
+      treeBase.appendChild(typeLi);
+    }
+
+    /*
+     *  Activate tree
+     */
+    let toggler = document.getElementsByClassName("caret");
+    for (let i = 0; i < toggler.length; i++) {
+      toggler[i].addEventListener("click", function() {
+        this.parentElement.querySelector(".nested").classList.toggle("active");
+        this.classList.toggle("caret-down");
+      });
     }
   }
 };
