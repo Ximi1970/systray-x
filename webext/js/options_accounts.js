0f64feed7b462f95cb85f65dd7f9d13045714b08
Ximi1970 <ximi.github@gmail.com>
2020-01-23 23:42:33 +0100

    Use const

--- webext/js/options_accounts.js
+++ webext/js/options_accounts.js
@@ -20,9 +20,8 @@ SysTrayX.Accounts = {
    */
   getAccountsCb: function(mailAccount) {
     let accounts = new Object();
-    let i;
 
-    for (i = 0; i < mailAccount.length; i++) {
+    for (let i = 0; i < mailAccount.length; i++) {
       console.debug("SysTrayX accounts id: " + mailAccount[i].id);
       console.debug("SysTrayX accounts name: " + mailAccount[i].name);
       console.debug("SysTrayX accounts type: " + mailAccount[i].type);
@@ -42,32 +41,32 @@ SysTrayX.Accounts = {
      */
 
     // Get base
-    let treeBase = document.getElementById("accountsTree");
+    const treeBase = document.getElementById("accountsTree");
 
     for (let prop in accounts) {
-      let typeLi = document.createElement("li");
+      const typeLi = document.createElement("li");
 
-      let typeSpan = document.createElement("span");
+      const typeSpan = document.createElement("span");
       if (accounts[prop]) {
         typeSpan.setAttribute("class", "caret");
       }
-      let typeText = document.createTextNode(prop);
+      const typeText = document.createTextNode(prop);
       typeSpan.appendChild(typeText);
       typeLi.appendChild(typeSpan);
 
       if (accounts[prop]) {
-        let typeUl = document.createElement("ul");
+        const typeUl = document.createElement("ul");
         typeUl.setAttribute("class", "nested active");
 
         for (let i = 0; i < accounts[prop].length; ++i) {
-          let typeLi = document.createElement("li");
-          let typeInput = document.createElement("input");
+          const typeLi = document.createElement("li");
+          const typeInput = document.createElement("input");
           typeInput.setAttribute("type", "checkbox");
           typeInput.setAttribute("name", accounts[prop][i].id);
           typeInput.setAttribute("value", JSON.stringify(accounts[prop][i]));
           typeInput.setAttribute("checked", "true");
           typeLi.appendChild(typeInput);
-          let typeText = document.createTextNode(" " + accounts[prop][i].name);
+          const typeText = document.createTextNode(" " + accounts[prop][i].name);
           typeLi.appendChild(typeText);
           typeUl.appendChild(typeLi);
         }
@@ -80,10 +79,10 @@ SysTrayX.Accounts = {
       function setAccounts(result) {
         console.debug("Restore account selection");
 
-        let treeBase = document.getElementById("accountsTree");
-        let accounts = result.accounts || [];
+        const treeBase = document.getElementById("accountsTree");
+        const accounts = result.accounts || [];
         for (let i = 0; i < accounts.length; ++i) {
-          let checkbox = treeBase.querySelector(
+          const checkbox = treeBase.querySelector(
             `input[name=${accounts[i].id}]`
           );
           if (checkbox) {
@@ -98,14 +97,14 @@ SysTrayX.Accounts = {
         console.log(`GetAccounts Error: ${error}`);
       }
 
-      let getAccounts = browser.storage.sync.get("accounts");
+      const getAccounts = browser.storage.sync.get("accounts");
       getAccounts.then(setAccounts, onError);
     }
 
     /*
      *  Activate tree
      */
-    let toggler = document.getElementsByClassName("caret");
+    const toggler = document.getElementsByClassName("caret");
     for (let i = 0; i < toggler.length; i++) {
       toggler[i].addEventListener("click", function() {
         this.parentElement.querySelector(".nested").classList.toggle("active");
--- webext/js/options_tabbutton.js
+++ webext/js/options_tabbutton.js
@@ -1,25 +1,24 @@
 function openTab(evt) {
-  var i, tabcontent, tablinks, tabName;
-  tabName = evt.currentTarget.id;
+  const tabName = evt.currentTarget.id;
 
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
+  const tabcontent = document.getElementsByClassName("tabcontent");
+  for (let i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
+  const tablinks = document.getElementsByClassName("tablinks");
+  for (let i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
 
-  let el = document.getElementById(tabName + "Content");
+  const el = document.getElementById(tabName + "Content");
   el.style.display = "block";
 
   evt.currentTarget.className += " active";
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-  let tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
+  const tablinks = document.getElementsByClassName("tablinks");
+  for (let i = 0; i < tablinks.length; i++) {
     document.getElementById(tablinks[i].id).addEventListener("click", openTab);
   }
 });
