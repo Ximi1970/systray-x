821db8ca45f523d23437ab69a08f6bd3e658418d
Ximi1970 <ximi.github@gmail.com>
2020-04-19 23:21:50 +0200

    Update installer for GNOME

--- dist/install.Fedora_31.sh
+++ dist/install.Fedora_31.sh
@@ -1,16 +1,21 @@
 enableGnomeExtension() {
     EXTENSION="appindicatorsupport@rgcjonas.gmail.com"
     PACKAGE="gnome-shell-extension-appindicator"
-    ENABLE_CMD="gnome-shell-extension-tool -e appindicatorsupport@rgcjonas.gmail.com"
+    ENABLE_CMD="gnome-shell-extension-tool -e ${EXTENSION}"
     #
     #   Is the extension installed?
     #
-    if [ -d /usr/share/gnome-shell/extensions/$EXTENSION ] || [ -d ~/.local/share/gnome-shell/extensions/$EXTENSION ] ; then
+    if [ -d /usr/share/gnome-shell/extensions/${EXTENSION} ] || [ -d ~/.local/share/gnome-shell/extensions/${EXTENSION} ] ; then
         $ENABLE_CMD
     else
-        echo "Please install: "$PACKAGE
-        echo "And run: "$ENABLE_CMD
+        mkdir -p ~/.local/share/gnome-shell/extensions
+        tar -C ~/.local/share/gnome-shell/extensions -xJf ${DESTINATION}/gnome-shell-extension.tar.xz
+        $ENABLE_CMD
     fi
+    
+    echo
+    echo "Please logout and login to activate the gnome shell extension"
+    echo
 }
 
 #
--- dist/install.openSUSE_Leap_15.2.sh
+++ dist/install.openSUSE_Leap_15.2.sh
@@ -1,16 +1,34 @@
 enableGnomeExtension() {
     EXTENSION="appindicatorsupport@rgcjonas.gmail.com"
     PACKAGE="gnome-shell-extension-appindicator"
-    ENABLE_CMD="gnome-shell-extension-tool -e appindicatorsupport@rgcjonas.gmail.com"
+    ENABLE_CMD="gnome-shell-extension-tool -e ${EXTENSION}"
+   
+    if [ ! -x /usr/bin/gnome-shell-extension-tool ] ; then
+      echo
+      echo "Please install the package gnome-shell-devel:"
+      echo
+      echo "sudo zypper install gnome-shell-devel"
+      echo
+      echo "And run after the insstaller is finished:"
+      echo
+      echo $ENABLE_CMD
+      echo
+    fi
+
     #
     #   Is the extension installed?
     #
-    if [ -d /usr/share/gnome-shell/extensions/$EXTENSION ] || [ -d ~/.local/share/gnome-shell/extensions/$EXTENSION ] ; then
+    if [ -d /usr/share/gnome-shell/extensions/${EXTENSION} ] || [ -d ~/.local/share/gnome-shell/extensions/${EXTENSION} ] ; then
         $ENABLE_CMD
     else
-        echo "Please install: "$PACKAGE
-        echo "And run: "$ENABLE_CMD
+        mkdir -p ~/.local/share/gnome-shell/extensions
+        tar -C ~/.local/share/gnome-shell/extensions -xJf ${DESTINATION}/gnome-shell-extension.tar.xz
+        $ENABLE_CMD
     fi
+    
+    echo
+    echo "Please logout and login to activate the gnome shell extension"
+    echo
 }
 
 #
