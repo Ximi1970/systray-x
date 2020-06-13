9f6bd898551cfd2141363eb7a96393b595295220
Ximi1970 <ximi.github@gmail.com>
2020-06-13 14:31:21 +0200

    Setup Qt translations

---
+++ app/SysTray-X/languages/SysTray-X_en.ts
@@ -0,0 +1,265 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>AboutDialog</name>
+    <message>
+        <location filename="../aboutdialog.ui" line="14"/>
+        <source>About SysTray-X</source>
+        <translation>About SysTray-X</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog.ui" line="26"/>
+        <source>SysTray-X Companion App by Ximi1970</source>
+        <translation>SysTray-X Companion App by Ximi1970</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog.ui" line="40"/>
+        <source>Version:</source>
+        <translation>Version:</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog.ui" line="47"/>
+        <source>0.0.1</source>
+        <translatorcomment>Do not translate</translatorcomment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog.ui" line="54"/>
+        <source>Build:</source>
+        <translation>Build:</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog.ui" line="61"/>
+        <location filename="../aboutdialog.ui" line="75"/>
+        <source>0</source>
+        <translatorcomment>Do not translate</translatorcomment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog.ui" line="68"/>
+        <source>Hash:</source>
+        <translation>Hash:</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog.ui" line="82"/>
+        <source>Branch:</source>
+        <translation>Branch:</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog.ui" line="89"/>
+        <source>master</source>
+        <translatorcomment>Do not translate</translatorcomment>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>DebugWidget</name>
+    <message>
+        <location filename="../debugwidget.ui" line="14"/>
+        <source>SysTray-X Debug</source>
+        <translation>SysTray-X Debug</translation>
+    </message>
+    <message>
+        <location filename="../debugwidget.ui" line="59"/>
+        <source>Test 2</source>
+        <translation>Test 2</translation>
+    </message>
+    <message>
+        <location filename="../debugwidget.ui" line="69"/>
+        <source>Unread mail:</source>
+        <translation>Unread mail:</translation>
+    </message>
+    <message>
+        <location filename="../debugwidget.ui" line="76"/>
+        <source>Test 1</source>
+        <translation>Test 1</translation>
+    </message>
+    <message>
+        <location filename="../debugwidget.ui" line="83"/>
+        <source>0</source>
+        <translatorcomment>Do not translate</translatorcomment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../debugwidget.ui" line="90"/>
+        <source>Test 3</source>
+        <translation>Test 3</translation>
+    </message>
+</context>
+<context>
+    <name>PreferencesDialog</name>
+    <message>
+        <location filename="../preferences.ui" line="14"/>
+        <source>SysTray-X Preferences</source>
+        <translation>SysTray-X Preferences</translation>
+    </message>
+    <message>
+        <location filename="../preferences.ui" line="26"/>
+        <source>Default icon</source>
+        <translation>Default icon</translation>
+    </message>
+    <message>
+        <location filename="../preferences.ui" line="32"/>
+        <source>Icon</source>
+        <translation>Icon</translation>
+    </message>
+    <message>
+        <location filename="../preferences.ui" line="40"/>
+        <source>Default Thunderbird</source>
+        <translation>Default Thunderbird</translation>
+    </message>
+    <message>
+        <location filename="../preferences.ui" line="53"/>
+        <source>Lookthrough icon</source>
+        <translation>Lookthrough icon</translation>
+    </message>
+    <message>
+        <location filename="../preferences.ui" line="65"/>
+        <location filename="../preferences.ui" line="188"/>
+        <source>Custom icon</source>
+        <translation>Custom icon</translation>
+    </message>
+    <message>
+        <location filename="../preferences.ui" line="82"/>
+        <location filename="../preferences.ui" line="205"/>
+        <source>Choose</source>
+        <translation>Choose</translation>
+    </message>
+    <message>
+        <location filename="../preferences.ui" line="99"/>
+        <source>KDE integration (Hide)</source>
+        <translation>KDE integration (Hide)</translation>
+    </message>
+    <message>
+        <location filename="../preferences.ui" line="112"/>
+        <source>Mail notification icon</source>
+        <translation>Mail notification icon</translation>
+    </message>
+    <message>
+        <location filename="../preferences.ui" line="125"/>
+        <source>Blank icon</source>
+        <translation>Blank icon</translation>
+    </message>
+    <message>
+        <location filename="../preferences.ui" line="158"/>
+        <source>New mail icon</source>
+        <translation>New mail icon</translation>
+    </message>
+    <message>
+        <location filename="../preferences.ui" line="219"/>
+        <source>No icon</source>
+        <translation>No icon</translation>
+    </message>
+    <message>
+        <location filename="../preferences.ui" line="266"/>
+        <source>Windows</source>
+        <translation>Windows</translation>
+    </message>
+    <message>
+        <location filename="../preferences.ui" line="274"/>
+        <source>Minimize</source>
+        <translation>Minimize</translation>
+    </message>
+    <message>
+        <location filename="../preferences.ui" line="282"/>
+        <source>Default minimize</source>
+        <translation>Default minimize</translation>
+    </message>
+    <message>
+        <location filename="../preferences.ui" line="295"/>
+        <source>Minimize to tray, method 1</source>
+        <translation>Minimize to tray, method 1</translation>
+    </message>
+    <message>
+        <location filename="../preferences.ui" line="308"/>
+        <source>Minimize to tray, method 2</source>
+        <translation>Minimize to tray, method 2</translation>
+    </message>
+    <message>
+        <location filename="../preferences.ui" line="325"/>
+        <source>Start application minimized</source>
+        <translation>Start application minimized</translation>
+    </message>
+    <message>
+        <location filename="../preferences.ui" line="332"/>
+        <source>Minimize on close</source>
+        <translation>Minimize on close</translation>
+    </message>
+    <message>
+        <location filename="../preferences.ui" line="349"/>
+        <source>Number properties</source>
+        <translation>Number properties</translation>
+    </message>
+    <message>
+        <location filename="../preferences.ui" line="357"/>
+        <source>Display unread message count</source>
+        <translation>Display unread message count</translation>
+    </message>
+    <message>
+        <location filename="../preferences.ui" line="369"/>
+        <source>Number color</source>
+        <translation>Number color</translation>
+    </message>
+    <message>
+        <location filename="../preferences.ui" line="390"/>
+        <source>Number size</source>
+        <translation>Number size</translation>
+    </message>
+    <message>
+        <location filename="../preferences.ui" line="412"/>
+        <source>Count type</source>
+        <translation>Count type</translation>
+    </message>
+    <message>
+        <location filename="../preferences.ui" line="420"/>
+        <source>Unread</source>
+        <translation>Unread</translation>
+    </message>
+    <message>
+        <location filename="../preferences.ui" line="433"/>
+        <source>New</source>
+        <translation>New</translation>
+    </message>
+    <message>
+        <location filename="../preferences.ui" line="470"/>
+        <source>Display debug window</source>
+        <translation>Display debug window</translation>
+    </message>
+    <message>
+        <location filename="../preferencesdialog.cpp" line="356"/>
+        <location filename="../preferencesdialog.cpp" line="380"/>
+        <source>Open Image</source>
+        <translation>Open Image</translation>
+    </message>
+    <message>
+        <location filename="../preferencesdialog.cpp" line="356"/>
+        <location filename="../preferencesdialog.cpp" line="380"/>
+        <source>Image Files (*.png *.jpg *.bmp)</source>
+        <translation>Image Files (*.png *.jpg *.bmp)</translation>
+    </message>
+</context>
+<context>
+    <name>SysTrayX</name>
+    <message>
+        <location filename="../systrayx.cpp" line="182"/>
+        <source>&amp;Show/Hide</source>
+        <translation>&amp;Show/Hide</translation>
+    </message>
+    <message>
+        <location filename="../systrayx.cpp" line="186"/>
+        <source>&amp;Preferences</source>
+        <translation>&amp;Preferences</translation>
+    </message>
+    <message>
+        <location filename="../systrayx.cpp" line="190"/>
+        <source>&amp;About</source>
+        <translation>&amp;About</translation>
+    </message>
+    <message>
+        <location filename="../systrayx.cpp" line="194"/>
+        <source>&amp;Quit</source>
+        <translation>&amp;Quit</translation>
+    </message>
+</context>
+</TS>
