92de63a88d0a4b9437052f2052ab2bc8eea34cff
Ximi1970 <ximi.github@gmail.com>
2020-11-22 17:57:39 +0100

    Fix version script

--- update_tb_version.sh
+++ update_tb_version.sh
@@ -24,13 +24,13 @@ SERVICE_FILE="./dist/deb/minimal/debian.control"
 sed -i -e "s?\(^Depends:.*, thunderbird (<= 1:\).*?\1$VERSION)?" $SERVICE_FILE
 
 SERVICE_FILE="./dist/arch/kde/PKGBUILD"
-sed -i -e "s?\(^.*thunderbird<=\).*?\1$VERSION?" $SERVICE_FILE
+sed -i -e "s?\(^.*thunderbird<=\).*?\1$VERSION'?" $SERVICE_FILE
 
 SERVICE_FILE="./dist/arch/gnome/PKGBUILD"
-sed -i -e "s?\(^.*thunderbird<=\).*?\1$VERSION?" $SERVICE_FILE
+sed -i -e "s?\(^.*thunderbird<=\).*?\1$VERSION'?" $SERVICE_FILE
 
 SERVICE_FILE="./dist/arch/minimal/PKGBUILD"
-sed -i -e "s?\(^.*thunderbird<=\).*?\1$VERSION?" $SERVICE_FILE
+sed -i -e "s?\(^.*thunderbird<=\).*?\1$VERSION'?" $SERVICE_FILE
 
 MANIFEST_FILE="./webext/manifest.json"
 sed -i -e "s?\(^.*\"strict_max_version\": \"\).*\(\.\*\"\)?\1$VERSION\2?" $MANIFEST_FILE
