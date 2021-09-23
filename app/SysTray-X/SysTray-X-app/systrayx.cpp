c606c72e84d53d0a57d509bfa0d70c0caacf5cda
Ximi1970 <ximi.github@gmail.com>
2021-09-23 23:45:00 +0200

    Minimize pref dialog

--- app/SysTray-X/SysTray-X-app/systrayx.cpp
+++ app/SysTray-X/SysTray-X-app/systrayx.cpp
@@ -72,6 +72,7 @@ SysTrayX::SysTrayX( QObject *parent ) : QObject( parent )
      *  Setup preferences dialog
      */
     m_pref_dialog = new PreferencesDialog( m_link, m_preferences );
+    m_pref_dialog->setFixedHeight( m_pref_dialog->sizeHint().height() );
 
     /*
      *  Setup debug window
