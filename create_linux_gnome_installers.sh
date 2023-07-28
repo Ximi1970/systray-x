1e529801fa8c715961ff033ac049ab64ebf87fd1
Ximi1970 <ximi.github@gmail.com>
2023-07-29 00:45:07 +0200

    Update installers

--- create_linux_gnome_installers.sh
+++ create_linux_gnome_installers.sh
@@ -69,12 +69,18 @@ OBS_DEB_GNOME_EXT+="v34 "
 OBS_DEB_ARCHS+="Debian_11/amd64 "
 OBS_DEB_PKS+="deb11 "
 OBS_DEB_GNOME_EXT+="v34 "
+OBS_DEB_ARCHS+="Debian_12/i386 "
+OBS_DEB_PKS+="deb12 "
+OBS_DEB_GNOME_EXT+="v46 "
+OBS_DEB_ARCHS+="Debian_12/amd64 "
+OBS_DEB_PKS+="deb12 "
+OBS_DEB_GNOME_EXT+="v46 "
 OBS_DEB_ARCHS+="Debian_Unstable/amd64 "
 OBS_DEB_PKS+="debunstable "
-OBS_DEB_GNOME_EXT+="v34 "
+OBS_DEB_GNOME_EXT+="v46 "
 OBS_DEB_ARCHS+="Debian_Testing/amd64 "
 OBS_DEB_PKS+="debtest "
-OBS_DEB_GNOME_EXT+="v34 "
+OBS_DEB_GNOME_EXT+="v46 "
 OBS_DEB_ARCHS+="xUbuntu_16.04/i386 "
 OBS_DEB_PKS+="xenial1604 "
 OBS_DEB_GNOME_EXT+="v26 "
@@ -126,6 +132,7 @@ create_gnome_extension_tar() {
   
   if [ ${GNOME_EXT} != "v26" ] &&
     [ ${GNOME_EXT} != "v34" ] &&
+    [ ${GNOME_EXT} != "v46" ] &&
     [ ${GNOME_EXT} != "master" ] ; then
     return
   fi
@@ -196,6 +203,9 @@ create_rpm_tar() {
   fi
   if [ "${GNOME_EXT}" == "v34" ] && [ -f gnome-shell-extension-v34.tar.xz ] ; then
     cp -f gnome-shell-extension-v34.tar.xz ${TAR_DIR}/gnome-shell-extension.tar.xz
+  fi
+  if [ "${GNOME_EXT}" == "v46" ] && [ -f gnome-shell-extension-v46.tar.xz ] ; then
+    cp -f gnome-shell-extension-v46.tar.xz ${TAR_DIR}/gnome-shell-extension.tar.xz
   fi
   if [ "${GNOME_EXT}" == "master" ] && [ -f gnome-shell-extension-master.tar.xz ] ; then
     cp -f gnome-shell-extension-master.tar.xz ${TAR_DIR}/gnome-shell-extension.tar.xz
@@ -334,6 +344,9 @@ create_deb_tar() {
   if [ "${GNOME_EXT}" == "v34" ] && [ -f gnome-shell-extension-v34.tar.xz ] ; then
     cp -f gnome-shell-extension-v34.tar.xz ${TAR_DIR}/gnome-shell-extension.tar.xz
   fi
+  if [ "${GNOME_EXT}" == "v46" ] && [ -f gnome-shell-extension-v46.tar.xz ] ; then
+    cp -f gnome-shell-extension-v46.tar.xz ${TAR_DIR}/gnome-shell-extension.tar.xz
+  fi
   if [ "${GNOME_EXT}" == "master" ] && [ -f gnome-shell-extension-master.tar.xz ] ; then
     cp -f gnome-shell-extension-master.tar.xz ${TAR_DIR}/gnome-shell-extension.tar.xz
   fi
@@ -430,6 +443,9 @@ create_pac_tar() {
   fi
   if [ "${GNOME_EXT}" == "v34" ] && [ -f gnome-shell-extension-v34.tar.xz ] ; then
     cp -f gnome-shell-extension-v34.tar.xz ${TAR_DIR}/gnome-shell-extension.tar.xz
+  fi
+  if [ "${GNOME_EXT}" == "v46" ] && [ -f gnome-shell-extension-v46.tar.xz ] ; then
+    cp -f gnome-shell-extension-v46.tar.xz ${TAR_DIR}/gnome-shell-extension.tar.xz
   fi
   if [ "${GNOME_EXT}" == "master" ] && [ -f gnome-shell-extension-master.tar.xz ] ; then
     cp -f gnome-shell-extension-master.tar.xz ${TAR_DIR}/gnome-shell-extension.tar.xz
